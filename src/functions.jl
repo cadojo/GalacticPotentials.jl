28f3300052bf4244b080bf163f82084bc6d4edef
Joey Carpinelli <joey@loopy.codes>
2024-06-22 21:02:21 +0000

    Adds derivatives, but doesn't work

--- src/functions.jl
+++ src/functions.jl
@@ -12,3 +12,17 @@ end
 
 @register_symbolic lowergamma(x, y)
 @register_symbolic gamma(x)
+
+function ModelingToolkit.derivative(::typeof(gamma), args::NTuple{1, Any}, ::Val{1})
+    return ForwardDiff.derivative(gamma, first(args))
+end
+
+function Symbolics.derivative(::typeof(lowergamma), args::NTuple{2, Any}, ::Val{1})
+    x, y = args
+    return ForwardDiff.derivative(x -> lowergamma(x, y), x)
+end
+
+function Symbolics.derivative(::typeof(lowergamma), args::NTuple{2, Any}, ::Val{2})
+    x, y = args
+    return ForwardDiff.derivative(y -> lowergamma(x, y), y)
+end
